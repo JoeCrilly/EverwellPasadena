b36d040eb34e6a1297d4af54fbb510da93bcc62b
saba <s.yaghoobi.s@gmail.com>
2016-02-05 13:45:46 -0800

    preloader v2

---
+++ javascript/Preloader.js
@@ -0,0 +1,8 @@
+
+	//<![CDATA[
+		$(window).load(function() { // makes sure the whole site is loaded
+			$('#status').fadeOut(); // will first fade out the loading animation
+			$('#preloader').delay(500).fadeOut('very slow'); // will fade out the white DIV that covers the website.
+			$('body').delay(500).css({'overflow':'visible'});
+		})
+	//]]>
