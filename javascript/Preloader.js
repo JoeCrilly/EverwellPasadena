2bef49f3fa86fa9a6df9d61e7294999dbc9081ea
JoeCrilly <joe.crilly503@gmail.com>
2016-03-11 09:58:19 -0800

    Pre loader changes

--- javascript/Preloader.js
+++ javascript/Preloader.js
@@ -1,8 +1,8 @@
 
 	//<![CDATA[
 		$(window).load(function() { // makes sure the whole site is loaded
-			$('#status').fadeOut(); // will first fade out the loading animation
-			$('#preloader').delay(500).fadeOut('very slow'); // will fade out the white DIV that covers the website.
-			$('body').delay(500).css({'overflow':'visible'});
+			$('#status').delay(3000).fadeOut(); // will first fade out the loading animation
+			$('#preloader').delay(3000).fadeOut('very slow'); // will fade out the white DIV that covers the website.
+			$('body').delay(3000).css({'overflow':'visible'});
 		})
 	//]]>
