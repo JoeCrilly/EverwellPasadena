bd7348e08cdf6651a25388543c7634b654d2f9f3
JoeCrilly <joe.crilly503@gmail.com>
2016-03-25 12:53:29 -0700

    Preloader

--- javascript/Preloader.js
+++ javascript/Preloader.js
@@ -1,8 +1,8 @@
 
 	//<![CDATA[
 		$(window).load(function() { // makes sure the whole site is loaded
-			$('#status').delay(3000).fadeOut(); // will first fade out the loading animation
-			$('#preloader').delay(3000).fadeOut('very slow'); // will fade out the white DIV that covers the website.
-			$('body').delay(3000).css({'overflow':'visible'});
+			$('#status').delay(2000).fadeOut(); // will first fade out the loading animation
+			$('#preloader').delay(2000).fadeOut('very slow'); // will fade out the white DIV that covers the website.
+			$('body').delay(2000).css({'overflow':'visible'});
 		})
 	//]]>
