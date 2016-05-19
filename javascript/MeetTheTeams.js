944d5e5d9430401216f53f67914a2075f5029741
Carmen Ng <Carmen Ng>
2016-05-19 04:17:51 -0700

    carousel, dan bio, preloader

--- javascript/MeetTheTeams.js
+++ javascript/MeetTheTeams.js
@@ -2,7 +2,23 @@
       $('.teamslider').slick({
 	   infinite: true,
 	   slidesToShow: 3,
-       slidesToScroll: 3
+       slidesToScroll: 3,
+       responsive: [
+		    {
+		      breakpoint: 900,
+		      settings: {
+		        slidesToShow: 2,
+		        slidesToScroll: 2
+		      }
+		    },
+		    {
+		      breakpoint: 599,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1
+		      }
+		    }
+		  ]
 	   });
 	   });
 	   
